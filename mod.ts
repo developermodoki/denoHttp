44dca1edc89ed8cf525fa2e7819de637443157c3
2_ØØØØØØ1 <61893884+developermodoki@users.noreply.github.com>
2022-05-01 18:42:46 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,15 +1,15 @@
-import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.137.0/http/server.ts";
 
-interface ResponseType<T> {
+export interface ResponseType<T> {
   text:T,
-  header?: Headers | null,
-  status?:number | null,
+  header?: Headers,
+  status?:number,
 }
 const handle:{urlName:string, handler: (req:Request) => Promise<ResponseType<any>> | ResponseType<any>, pattern?:URLPattern}[] = [];
 const middleware:{fn:(req:Request) => void | {res:unknown} | Promise<{res:unknown} | void>}[] = []
 const globalHeaders:Headers | {} = {};
 
-class Server {
+export class Server {
   constructor(){}
 
   set(handler: (req:Request) => void | {res:unknown} | Promise<{res:unknown} | void>) {
@@ -25,10 +25,11 @@ class Server {
   setGlobalHeaders(headers:Headers):void {
     Object.assign(globalHeaders,headers);
   }
-
   async handler(req: Request): Promise<Response> {
+
     if(middleware.length !== 0){
       let reject: undefined | {res:any} = undefined;
+
       for await (const u of middleware) {
         const res = await u.fn(req);
         if(res !== undefined) {
@@ -36,7 +37,8 @@ class Server {
           break;
         }
       }
-      if(reject) return new Response(reject.res);
+
+      if(reject) return new Response(reject.res,{status:400,headers:globalHeaders});
     } 
     if(handle.length !== 0) {
       for await(const i of handle) {
@@ -47,15 +49,19 @@ class Server {
               status:(pendingRes.status ? pendingRes.status : 200),
               headers: (pendingRes.header ? Object.assign(pendingRes.header,globalHeaders) : Object.assign({"content-type": "application/json"},globalHeaders))
             });
+
           } catch(e) {
             console.error(e); return new Response("error"); 
           }
+
         } else {
           return new Response("Not Found",{status:404});
         }
       }
+
       return new Response("Not Found",{status:404});
     } else {
+
       return new Response("Erorr: handlers length is 0",{status:400});
     }
   }
@@ -63,12 +69,8 @@ class Server {
   async listen():Promise<void> {
     await serve(this.handler)
   }
+  getParams(pattern:string,url:string):{[key:string]:string} | null {
+    const result = new URLPattern(pattern).exec(url);
+    return (result ? result.pathname.groups : null);
+  }
 }
-const app = new Server();
-
-app.on("/testing/:vovid",(req): ResponseType<string> => {
-  console.log((req.text()))
-  console.log(req.headers)
-  return {text:"waa"}
-})
-app.listen();
